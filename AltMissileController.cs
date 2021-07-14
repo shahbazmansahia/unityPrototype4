d0dd15689be536033117861dba97809f072f6484
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-07-14 08:46:16 -0700

    Trying an alternate approach using the walkthrough now. Developed the AltMissileController and PowerUpType as a part of that approach. Done till step 7; am burnt out from last week so might take tomorrow off.

---
+++ AltMissileController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltMissileController : MonoBehaviour
+{
+    private Transform target;
+    private float speed = 15.0f;
+    private bool homing;
+
+    private float rocketStrength = 15.0f;
+    private float aliveTimer = 5.0f;
+
+    private void Update()
+    {
+        if (homing && target != null)
+        {
+            Vector3 moveDirection = (target.transform.position - transform.position).normalized;
+            transform.position += moveDirection * speed * Time.deltaTime;
+            transform.LookAt(target);
+        }   
+    }
+
+    public void Fire (Transform newTarget)
+    {
+        target = newTarget;
+        homing = true;
+        Destroy(gameObject, aliveTimer);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (target != null)
+        {
+            if (other.gameObject.CompareTag("target.tag"))
+            {
+                Rigidbody targetRigidbody = other.gameObject.GetComponent<Rigidbody>();
+                Vector3 away = -other.contacts[0].normal;
+                targetRigidbody.AddForce(away * rocketStrength, ForceMode.Impulse);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ PowerUp.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PowerUpType { None, Pushback, Rockets }
+public class PowerUp : MonoBehaviour
+{
+    public PowerUpType powerUpType;
+}
